e715caa9b79380d80442fc1b6629dc90285fef39
Simon Christmann <simon@christmann.email>
2020-09-22 12:14:44 +0200

    rm “mqtt-smarthome”-specific Topic Replacements

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ mqtt.on('message', (topic, payload, msg) => {
         const match = mqttWildcard(topic, subs.topic);
 
         if (match && typeof options.condition === 'function') {
-            if (!options.condition(topic.replace(/^([^/]+)\/status\/(.+)/, '$1//$2'), state.val, state, oldState, msg)) {
+            if (!options.condition(topic, state.val, state, oldState, msg)) {
                 return;
             }
         }
@@ -193,7 +193,7 @@ mqtt.on('message', (topic, payload, msg) => {
                  * @param {object} objPrev - previous state - the whole state object
                  * @param {object} msg - the mqtt message as received from MQTT.js
                  */
-                subs.callback(topic.replace(/^([^/]+)\/status\/(.+)/, '$1//$2'), state.val, state, oldState, msg);
+                subs.callback(topic, state.val, state, oldState, msg);
             }, delay);
         }
     });
@@ -348,8 +348,6 @@ function runScript(script, name) {
             }
 
             if (typeof topic === 'string') {
-                topic = topic.replace(/^([^/]+)\/\//, '$1/status/');
-
                 if (typeof options.condition === 'string') {
                     if (options.condition.indexOf('\n') !== -1) {
                         throw new Error('options.condition string must be one-line javascript');
@@ -365,11 +363,11 @@ function runScript(script, name) {
                 subscriptions.push({topic, options, callback: (typeof callback === 'function') && scriptDomain.bind(callback)});
 
                 if (options.retain && status[topic] && typeof callback === 'function') {
-                    callback(topic.replace(/^([^/]+)\/status\/(.+)/, '$1//$2'), status[topic].val, status[topic]);
+                    callback(topic, status[topic].val, status[topic]);
                 } else if (options.retain && (/\/\+\//.test(topic) || /\+$/.test(topic) || /\+/.test(topic) || topic.endsWith('#')) && typeof callback === 'function') {
                     for (const t in status) {
                         if (mqttWildcard(t, topic)) {
-                            callback(t.replace(/^([^/]+)\/status\/(.+)/, '$1//$2'), status[t].val, status[t]);
+                            callback(t, status[t].val, status[t]);
                         }
                     }
                 }
@@ -450,8 +448,6 @@ function runScript(script, name) {
                 return;
             }
 
-            topic = topic.replace(/^([^/]+)\/\/(.+)$/, '$1/set/$2');
-
             if (typeof payload === 'object') {
                 payload = JSON.stringify(payload);
             } else {
@@ -465,7 +461,6 @@ function runScript(script, name) {
          * @returns {mixed} the topics value
          */
         getValue: function Sandbox_getValue(topic) {
-            topic = topic.replace(/^([^/]+)\/\/(.+)$/, '$1/status/$2');
             return status[topic] && status[topic].val;
         }
     };
